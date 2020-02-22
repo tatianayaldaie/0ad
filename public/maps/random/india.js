1d62932004054bfd0bb4099759074c8c0b671941
tatianayaldaie <47925343+tatianayaldaie@users.noreply.github.com>
2020-02-22 09:45:31 +0200

    added lions to Indian map
    Changed tigers with lions in Indian map. There are more lions in India then tigers

--- public/maps/random/india.js
+++ public/maps/random/india.js
@@ -11,7 +11,7 @@ const tWater = "savanna_riparian_wet";
 const oTree = "gaia/flora_tree_palm_tropic";
 const oBerryBush = "gaia/flora_bush_berry";
 const oRabbit = "gaia/fauna_rabbit";
-const oTiger = "gaia/fauna_tiger";
+const oLion = "gaia/fauna_lion";
 const oCrocodile = "gaia/fauna_crocodile";
 const oFish = "gaia/fauna_fish";
 const oElephant = "gaia/fauna_elephant_asian";
@@ -160,10 +160,10 @@ createObjectGroupsDeprecated(
 	scaleByMapSize(4, 12), 50
 );
 
-g_Map.log("Creating tigers");
+g_Map.log("Creating lions");
 createObjectGroupsDeprecated(
 	new SimpleGroup(
-		[new SimpleObject(oTiger, 2, 2, 0, 4)],
+		[new SimpleObject(oLion, 2, 2, 0, 4)],
 		true, clFood
 	),
 	0,
